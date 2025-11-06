0bcf6f2b684f46d7063bc6f39ad631d34d7abc29
dimanesterenko <nesterenkoodmytro@gmail.com>
2025-11-06 15:32:07 +0200

    First start comm

---
+++ app/db.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+asyncpg://user:password@localhost:5432/booking_db"
+)
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+async_session = sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession
+)
+
+Base = declarative_base()
